5967ff84f28f371232cf1b9cb3c8d736e2a7341a
Markus Haack <markus.haack@gmail.com>
2024-12-27 17:24:56 +0100

    fix: icons

--- src/_data/icons.js
+++ src/_data/icons.js
@@ -1,14 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+function readSvgFile(filename) {
+  const filePath = path.join(__dirname, '..', '_includes', 'icons', filename);
+  return fs.readFileSync(filePath, 'utf8');
+}
+
 module.exports = {
-  currency: '{% include "icons/currency.svg" %}',
-  math: '{% include "icons/math.svg" %}',
-  scriptNumbers: '{% include "icons/script-numbers.svg" %}',
-  fractions: '{% include "icons/fractions.svg" %}',
-  legal: '{% include "icons/legal.svg" %}',
-  punctuation: '{% include "icons/punctuation.svg" %}',
-  arrows: '{% include "icons/arrows.svg" %}',
-  diacritics: '{% include "icons/diacritics.svg" %}',
-  ligatures: '{% include "icons/ligatures.svg" %}',
-  weather: '{% include "icons/weather.svg" %}',
-  keyboard: '{% include "icons/keyboard.svg" %}',
-  misc: '{% include "icons/misc.svg" %}'
+  currency: readSvgFile('currency.svg'),
+  math: readSvgFile('math.svg'),
+  scriptNumbers: readSvgFile('script-numbers.svg'),
+  fractions: readSvgFile('fractions.svg'),
+  legal: readSvgFile('legal.svg'),
+  punctuation: readSvgFile('punctuation.svg'),
+  arrows: readSvgFile('arrows.svg'),
+  diacritics: readSvgFile('diacritics.svg'),
+  ligatures: readSvgFile('ligatures.svg'),
+  weather: readSvgFile('weather.svg'),
+  keyboard: readSvgFile('keyboard.svg'),
+  misc: readSvgFile('misc.svg')
 }; 
