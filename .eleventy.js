ac655ec824ff11a6e85a1967437d4a6125943fe8
Markus Haack <markus.haack@gmail.com>
2024-12-27 14:27:29 +0100

    fix: add footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
+    
+    // Add year shortcode
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   
     return {
       dir: {
