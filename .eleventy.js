9ece0adf2ecfd9dd60e108c8e2539092597dc3a0
Markus Haack <markus.haack@gmail.com>
2025-01-14 21:10:24 +0100

    fix: add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/favicon.svg");
     
     // Add year shortcode
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
