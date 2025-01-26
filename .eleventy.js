a341d1ae7870dc258013054d458e1711754b4a63
Markus Haack <markus.haack@gmail.com>
2025-01-26 14:05:33 +0100

    feat: minify HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const htmlmin = require("html-minifier-terser");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
@@ -5,6 +7,20 @@ module.exports = function(eleventyConfig) {
     
     // Add year shortcode
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+      if (process.env.NODE_ENV === "production" && outputPath && outputPath.endsWith(".html")) {
+        let minified = htmlmin.minify(content, {
+          removeComments: true,
+          collapseWhitespace: true,
+          minifyJS: true,
+          minifyCSS: true, 
+          useShortDoctype: true,
+        });
+        return minified;
+      }
+      return content;
+    });
   
     return {
       dir: {
